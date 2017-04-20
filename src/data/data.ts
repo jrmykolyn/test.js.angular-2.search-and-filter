ff4fb989efe4b18d873ba590e512b3290e2c827b
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-04-20 16:50:52 -0400

    Updated 'results data' to include 'type' info. Exposed 'Types' array.

--- src/data/data.ts
+++ src/data/data.ts
@@ -5,6 +5,13 @@ const titleStrings = [
   'Title'
 ];
 
+const typeObjects = [
+  { name: 'All', value: null },
+  { name: 'Article', value: 'article' },
+  { name: 'Event', value: 'event' },
+  { name: 'Place', value: 'place' }
+];
+
 const categoryObjects = [
   { name: 'All', value: null },
   { name: 'Politics', value: 'politics' },
@@ -15,15 +22,18 @@ const categoryObjects = [
 
 interface ResultInterface {
   title: string,
+  type: any,
   category: any
 };
 
 class Result implements ResultInterface {
   title: string;
+  type: any;
   category: any;
 
   constructor( data? ) {
     this.title = ( titleStrings.reduce( ( string1: string, string2: string ) => { return `${string1} ${string2}`; } ) ); /// TEMP
+    this.type = typeObjects[ Math.floor( Math.random() * typeObjects.length ) ];
     this.category = categoryObjects[ Math.floor( Math.random() * categoryObjects.length ) ];
   }
 }
@@ -33,6 +43,8 @@ var resultObjects = [];
 for ( let i = 0, x = 30; i < x; i++ ) {
     resultObjects.push( new Result() ); /// TEMP
 }
+
+export const Types = typeObjects;
 
 export const Categories = categoryObjects;
 
