05fbe1f0465b56db512a634afa05ec01751551c4
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-04-20 13:52:08 -0400

    Completed additional updates to 'results data'.

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,20 +1,28 @@
-export const Results = [
-  {
-    id: 1,
-    title: 'Test Title'
-  },
-  {
-    id: 2,
-    title: 'Test Title'
-  },
-  {
-    id: 3,
-    title: 'Test Title'
-  },
-  {
-    id: 4,
-    title: 'Test Title'
-  },
+const titleStrings = [
+  'This',
+  'Is',
+  'A',
+  'Title'
 ];
+
+interface ResultInterface {
+  title: string
+};
+
+class Result implements ResultInterface {
+  title: string;
+
+  constructor( data? ) {
+    this.title = ( titleStrings.reduce( ( string1: string, string2: string ) => { return `${string1} ${string2}`; } ) ); /// TEMP
+  }
+}
+
+var resultObjects = [];
+
+for ( let i = 0, x = 30; i < x; i++ ) {
+    resultObjects.push( new Result() ); /// TEMP
+}
+
+export const Results = resultObjects;
 
 export const ResultIds = Results.map( ( result: any ) => { return result.id } );
