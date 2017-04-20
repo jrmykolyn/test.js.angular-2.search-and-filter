70700802fe4ed8231e709a5cf26932a11148110e
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-04-20 12:11:27 -0400

    Started adding mock result data.

---
+++ src/data/data.ts
@@ -0,0 +1,20 @@
+export const Results = [
+  {
+    id: 1,
+    title: 'Test Title'
+  },
+  {
+    id: 2,
+    title: 'Test Title'
+  },
+  {
+    id: 3,
+    title: 'Test Title'
+  },
+  {
+    id: 4,
+    title: 'Test Title'
+  },
+];
+
+export const ResultIds = Results.map( ( result: any ) => { return result.id } );
