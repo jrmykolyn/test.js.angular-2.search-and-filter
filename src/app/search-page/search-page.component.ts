1bcd62202426c12154dda0978cf444e727ce6ea8
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-04-20 13:55:48 -0400

    Updated 'SearchPageComponent': imported 'results' data; displayed 1x 'PostPreviewComponent' for each result.

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Results, ResultIds } from '../../data/data';
+
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.css']
+  styleUrls: ['./search-page.component.scss']
 })
 export class SearchPageComponent implements OnInit {
+  results: Array<any> = [];
 
-  constructor() { }
+  constructor(
+  ) { }
 
   ngOnInit() {
+    this.results = Results;
   }
 
 }
